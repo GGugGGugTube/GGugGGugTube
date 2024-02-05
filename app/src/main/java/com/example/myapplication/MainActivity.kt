deb9629a8c739a7dabe9fe6885927a8f51ad02af
kiwon kim <rlarldnjs17@gmail.com>
2024-02-05 20:59:49 +0900

    Merge: Pull

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        initBottomNav()
+    }
+
+    private fun initBottomNav() {
+        binding.bottomNav.run {
+            setOnItemSelectedListener {
+                when(it.itemId) {
+                }
+            }
+        }
     }
 }
