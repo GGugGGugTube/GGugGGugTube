ad830bf543622d7feaac776e92ee6eeb7fa326a6
sunjoolee <sheisthepoem@naver.com>
2024-02-14 22:35:12 +0900

    Feat: 좋아요 클릭 처리 리스너

--- app/src/main/java/com/example/myapplication/like/LikedUtils.kt
+++ app/src/main/java/com/example/myapplication/like/LikedUtils.kt
@@ -1,23 +1,33 @@
 package com.example.myapplication.like
 
 import android.content.Context
+import com.example.myapplication.MyApplication
 import com.example.myapplication.YoutubeVideo
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
 object LikedUtils {
+    fun saveLikedVideos(videos: List<YoutubeVideo>) {
+        val context = MyApplication.appContext!!
 
-    fun saveLikedVideos(context: Context, videos: List<YoutubeVideo>) {
         val prefs =
-            context.getSharedPreferences(Liked.LIKED_VIDEOS_PREFS_NAME, Context.MODE_PRIVATE)
+            context.getSharedPreferences(
+                LikedConstants.LIKED_VIDEOS_PREFS_NAME,
+                Context.MODE_PRIVATE
+            )
         val json = Gson().toJson(videos)
-        prefs.edit().putString(Liked.LIKED_VIDEOS_PREF_KEY, json).apply()
+        prefs.edit().putString(LikedConstants.LIKED_VIDEOS_PREF_KEY, json).apply()
     }
 
-    fun getLikedVideos(context: Context): List<YoutubeVideo> {
+    fun getLikedVideos(): List<YoutubeVideo> {
+        val context = MyApplication.appContext!!
+
         val prefs =
-            context.getSharedPreferences(Liked.LIKED_VIDEOS_PREFS_NAME, Context.MODE_PRIVATE)
-        val json = prefs.getString(Liked.LIKED_VIDEOS_PREF_KEY, null)
+            context.getSharedPreferences(
+                LikedConstants.LIKED_VIDEOS_PREFS_NAME,
+                Context.MODE_PRIVATE
+            )
+        val json = prefs.getString(LikedConstants.LIKED_VIDEOS_PREF_KEY, null)
         return if (json != null) {
             val type = object : TypeToken<List<YoutubeVideo>>() {}.type
             Gson().fromJson(json, type)
