e33db35febfeeb9181fa74a2b368c0aa6d1e46ba
hyunparrot <coyh1071@gmail.com>
2024-02-14 12:54:32 +0900

    Feat WatchList ------------------------------------------ Remove: Delete code not used Feat: watchList package
    1. 검색기록 오브젝트 추가
    2. 검색기록 가져오는 코드 추가

--- app/src/main/java/com/example/myapplication/watchlist/WatchListUtils.kt
+++ app/src/main/java/com/example/myapplication/watchlist/WatchListUtils.kt
@@ -8,13 +8,15 @@ import com.google.gson.reflect.TypeToken
 object WatchListUtils {
 
     fun saveWatchList(context: Context, videos: List<YoutubeVideo>) {
-        val prefs = context.getSharedPreferences(WatchList.WATCH_LIST_PREFS_NAME, Context.MODE_PRIVATE)
+        val prefs =
+            context.getSharedPreferences(WatchList.WATCH_LIST_PREFS_NAME, Context.MODE_PRIVATE)
         val json = Gson().toJson(videos)
         prefs.edit().putString(WatchList.WATCH_LIST_PREF_KEY, json).apply()
     }
 
     fun getWatchList(context: Context): List<YoutubeVideo> {
-        val prefs = context.getSharedPreferences(WatchList.WATCH_LIST_PREFS_NAME, Context.MODE_PRIVATE)
+        val prefs =
+            context.getSharedPreferences(WatchList.WATCH_LIST_PREFS_NAME, Context.MODE_PRIVATE)
         val json = prefs.getString(WatchList.WATCH_LIST_PREF_KEY, null)
         return if (json != null) {
             val type = object : TypeToken<List<YoutubeVideo>>() {}.type
@@ -25,7 +27,8 @@ object WatchListUtils {
     }
 
     fun clearWatchList(context: Context) {
-        val prefs = context.getSharedPreferences(WatchList.WATCH_LIST_PREFS_NAME, Context.MODE_PRIVATE)
+        val prefs =
+            context.getSharedPreferences(WatchList.WATCH_LIST_PREFS_NAME, Context.MODE_PRIVATE)
         prefs.edit().remove(WatchList.WATCH_LIST_PREF_KEY).apply()
     }
 }
