28cebdb42cbef1141c335ecfc01d76bd7d9aa59f
sunjoolee <sheisthepoem@naver.com>
2024-02-14 11:59:06 +0900

    merge: Merged from dev

---
+++ app/src/main/java/com/example/myapplication/splash/Splash.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.myapplication.MainActivity
+import com.example.myapplication.R
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        loadSplashScreen()
+    }
+    private fun loadSplashScreen(){
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
